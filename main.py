664ec67fe739d904a4b9ac26c572a1c131e41031
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-06-20 19:39:39 +0100

    viewing in conected device

--- main.py
+++ main.py
@@ -24,5 +24,14 @@ def index():
 def video_feed():
     return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# THIS to view output on this system
+
+#if __name__ == "__main__":
+    #app.run(debug=True)
+
+    # OR
+
+# to access the streaming on another device on the same network
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+# Then connect to same wifi and input IP_OF_MY_DEVICE/5001
